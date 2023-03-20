1a77b0238167ff4a4be3d391256948feacc23e3b
James Rogers <jd2rogers2@gmail.com>
2023-03-20 19:12:50 +0000

    wrote form component with fetch req #1

---
+++ ghi/src/CreateTodoItem.js
@@ -0,0 +1,37 @@
+import { useEffect, useState } from "react";
+import Construct from "./Construct.js";
+import ErrorNotification from "./ErrorNotification";
+import "./App.css";
+
+function CreateTodoItem() {
+  const [launch_info, setLaunchInfo] = useState([]);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    async function getData() {
+      let url = `${process.env.REACT_APP_SAMPLE_SERVICE_API_HOST}/api/launch-details`;
+      console.log("fastapi url: ", url);
+      let response = await fetch(url);
+      console.log("------- hello? -------");
+      let data = await response.json();
+
+      if (response.ok) {
+        console.log("got launch data!");
+        setLaunchInfo(data.launch_details);
+      } else {
+        console.log("drat! something happened");
+        setError(data.message);
+      }
+    }
+    getData();
+  }, []);
+
+  return (
+    <div>
+      <ErrorNotification error={error} />
+      <Construct info={launch_info} />
+    </div>
+  );
+}
+
+export default CreateTodoItem;

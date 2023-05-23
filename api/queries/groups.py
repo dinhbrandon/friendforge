0642bc916fe1c42b564d28940b0f908ccfb036e8
Ramon <ramon.duarte55@gmail.com>
2023-05-22 18:27:05 -0700

    working on create group

--- api/queries/groups.py
+++ api/queries/groups.py
@@ -8,45 +8,83 @@ class Error(BaseModel):
 
 
 class GroupIn(BaseModel):
-    focus: str
+    focus_id: int
     name: str
     icon_photo: str
-    user_profile: str
-    chatroom: str
+    chatroom_id: str
 
 
 class GroupOut(BaseModel):
     id: int
-    focus: str
+    focus_id: int
     name: str
     icon_photo: str
-    user_profile: str
-    chatroom: str
+    chatroom_id: str
+
+
+class GroupUpdateIn(BaseModel):
+    name: str
+    icon_photo: str
 
 
 class GroupRepository:
-    def get_groups(self) -> Union[Error, List[GroupOut]]:
+    # def get_groups(self) -> Union[Error, List[GroupOut]]:
+    #     try:
+    #         with pool.connection() as conn:
+    #             with conn.cursor() as db:
+    #                 result = db.execute(
+    #                     """
+    #                     SELECT id, focus_id, name, icon_photo, user_profile, chatroom_id
+    #                     from groups
+    #                     ORDER BY id;
+    #                     """
+    #                 )
+    #                 return [
+    #                     GroupOut(
+    #                         id=record[0],
+    #                         focus_id=record[1],
+    #                         name=record[2],
+    #                         icon_photo=record[3],
+    #                         user_profile=record[4],
+    #                         chatroom_id=record[5],
+    #                     )
+    #                     for record in db
+    #                 ]
+    #     except Exception as e:
+    #         print(e)
+    #         return {"message": "Could not get groups"}
+    def update(self, group_id) -> Union[GroupUpdateIn, Error]:
+        
+
+    def create(self, group: GroupIn) -> GroupOut:
         try:
             with pool.connection() as conn:
                 with conn.cursor() as db:
                     result = db.execute(
                         """
-                        SELECT id, focus, name, icon_photo, user_profile, chatroom
-                        from groups
-                        ORDER BY id;
-                        """
-                    )
-                    return [
-                        GroupOut(
-                            id=record[0],
-                            focus=record[1],
-                            name=record[2],
-                            icon_photo=record[3],
-                            user_profile=record[4],
-                            chatroom=record[4],
+                        INSERT INTO groups (
+                            focus_id,
+                            name,
+                            icon_photo,
+                            chatroom_id
                         )
-                        for record in db
-                    ]
+                        VALUES (%s, %s, %s, %s)
+                        RETURNING id;
+                        """,
+                        [
+                            group.focus_id,
+                            group.name,
+                            group.icon_photo,
+                            group.chatroom_id,
+                        ]
+                    )
+                    print("testing")
+                    id = result.fetchone()[0]
+                    return self.group_in_to_out(id, group)
         except Exception as e:
             print(e)
-            return {"message": "Could not get groups"}
+            return {"message": "could not create group"}
+
+    def group_in_to_out(self, id: int, group: GroupIn):
+        old_data = group.dict()
+        return GroupOut(id=id, **old_data)
--- api/routers/groups.py
+++ api/routers/groups.py
@@ -1,4 +1,5 @@
 from fastapi import APIRouter, Depends, Response
+from authenticator import authenticator
 from typing import Union, List, Optional
 from queries.groups import (
     Error,
@@ -15,3 +16,23 @@ def get_groups(
     repo: GroupRepository = Depends(),
 ):
     return repo.get_groups()
+
+
+@router.post("/groups", response_model=Union[GroupOut, Error])
+def create_group(
+    group: GroupIn,
+    response: Response,
+    repo: GroupRepository = Depends(),
+    # account_data: dict = Depends(authenticator.get_current_account_data)
+):
+    print(group)
+    return repo.create(group)
+
+@router.put("/groups/{group_id}", response_model=Union[GroupOut, Error])
+def edit_group(
+    group_id: int,
+    group: GroupIn,
+    repo: GroupRepository = Depends(),
+    account_data: dict = Depends(authenticator.get_current_account_data)
+):
+    return repo.update(group_id, group)

c072f78a4d7352b727cb1021c98de1a48f1abaf6
Taylor <taywayne@outlook.com>
2023-05-30 13:00:03 -0700

    useProfile function working

--- ghi/src/components/headerauth/HeaderToken.js
+++ ghi/src/components/headerauth/HeaderToken.js
@@ -1,24 +1,30 @@
-import React from 'react'
+import React from "react";
 import useToken from "@galvanize-inc/jwtdown-for-react";
-import HeaderGroups from './HeaderGroups'
+import HeaderGroups from "./HeaderGroups";
 import { useNavigate } from "react-router-dom";
-import useUser from '../useUser'
+import useUser from "../useUser";
+import useProfile from "../useProfile";
 
 function HeaderToken() {
-  const { token, logout } = useToken()
-  const { user } = useUser(token)
+  const { token, logout } = useToken();
+  const { user } = useUser(token);
+  const { profile } = useProfile(token);
   const navigate = useNavigate();
 
   const handleLogout = () => {
-    logout()
-    navigate('/')
-  }
+    logout();
+    navigate("/");
+  };
 
   return (
     <>
       <ul className="menu menu-horizontal px-1 text-base-content">
-        <li><a href='/'>Home</a></li>
-        <li><a href='/forge'>Forge</a></li>
+        <li>
+          <a href="/">Home</a>
+        </li>
+        <li>
+          <a href="/forge">Forge</a>
+        </li>
       </ul>
       <HeaderGroups />
       <div className="dropdown dropdown-end">
@@ -27,15 +33,25 @@ function HeaderToken() {
             <img src="" />
           </div>
         </label>
-        <ul tabIndex="0" className="mt-3 p-2 shadow menu menu-compact dropdown-content bg-neutral rounded-box w-52 text-base-content">
+        <ul
+          tabIndex="0"
+          className="mt-3 p-2 shadow menu menu-compact dropdown-content bg-neutral rounded-box w-52 text-base-content"
+        >
           <li>Welcome {user.username}</li>
-          <li><a href=''>Profile</a></li>
-          <li><a href=''>Settings</a></li>
-          <li><a onClick={handleLogout}>Logout</a></li>
+          <li>you live in {profile.location}</li>
+          <li>
+            <a href="">Profile</a>
+          </li>
+          <li>
+            <a href="">Settings</a>
+          </li>
+          <li>
+            <a onClick={handleLogout}>Logout</a>
+          </li>
         </ul>
       </div>
     </>
-  )
+  );
 }
 
-export default HeaderToken
+export default HeaderToken;

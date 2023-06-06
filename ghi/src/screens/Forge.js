66f2ef0b7fdc68260be3553ceaa0fbd047f4bfa3
Brandon Dinh <dinhbrandon@gmail.com>
2023-06-06 17:43:16 +0000

    Matching algorithms

--- ghi/src/screens/Forge.js
+++ ghi/src/screens/Forge.js
@@ -1,18 +1,21 @@
 import React from 'react'
 import GroupCard from '../components/forge/groupcard'
 import './style/forge.css'
+import people from "../components/forge/people.png";
+import ForgeGroup from '../components/forge/enterforge';
 
 function Forge() {
   return (
     <>
-    <div className='top-body'>
-      <h2>Forge!</h2>
-      <p>
-        This is the forge.
-      </p>
-    </div>
-    <div className='card-diplay'>
-      <GroupCard />
+    <div class="h-screen flex items-center justify-center">
+      <div className="card w-96 glass">
+        <figure><img src={people}/></figure>
+          <div className="card-body">
+            <h2 className="card-title">Welcome to the forge</h2>
+              <p>Select a group focus</p>
+                <ForgeGroup />
+          </div>
+      </div>
     </div>
 
     </>

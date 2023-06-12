d443d7c912a6a616f6994f35903c95f1c0fb12bd
Brandon Dinh <dinhbrandon@gmail.com>
2023-06-12 15:37:41 -0700

    created migration table for friend request

--- api/migrations/006_migrations_6.py
+++ api/migrations/006_migrations_6.py
@@ -13,5 +13,24 @@ steps = [
         """
         DROP TABLE friendships;
         """
+    ],
+    [
+        """
+        CREATE TABLE friend_requests (
+            id SERIAL PRIMARY KEY NOT NULL,
+            sender_id INTEGER NOT NULL REFERENCES user_profile (id) ON DELETE CASCADE,
+            receiver_id INTEGER NOT NULL REFERENCES user_profile (id) ON DELETE CASCADE,
+            message VARCHAR(1000),
+            status VARCHAR(100) NOT NULL,
+            created_at TIMESTAMP NOT NULL,
+            deleted_at TIMESTAMP,
+            CHECK (sender_id <> receiver_id),
+            UNIQUE (sender_id, receiver_id)
+        );
+        """,
+
+        """
+        DROP TABLE friendships;
+        """
     ]
 ]
